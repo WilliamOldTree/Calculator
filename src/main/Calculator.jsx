b5ca44e864e9051c1e574ea9003dcae301b1677d
will <denis.barbosa@fatec.sp.gov.br>
2022-03-02 19:45:56 -0300

    Finalizado

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -32,7 +32,30 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation);
+        if(this.setState.current === 0){
+            this.setState({ operation, current: 1, clearDisplay: true});
+        }else{
+            const equals = operation === '='
+            const currentOperation = this.state.operational;
+
+            const values = [...this.state.values];
+           try{
+            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);   
+           } catch(e){
+            values[0] = this.state.values[0];   
+           }
+           
+            values[1] = 0;
+
+            this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals,
+            values
+
+            })
+        }
     }
 
     addDigit(n) {
@@ -40,10 +63,20 @@ export default class Calculator extends Component {
             return
         }
 
-        const clearDisplay = this.state.display === '0' || this.state.clearDisplay;
+        const clearDisplay = this.state.displayValue === '0'
+         || this.state.clearDisplay;
         const currentValue = clearDisplay ? '' : this.state.displayValue;
         const displayValue = currentValue + n;
         this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.'){
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values });
+            console.log(values)
+        }
     }
 
 
